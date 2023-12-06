426e2174c31f996f0dfa60f2b03ac52a2238cfc0
Samson Ameh <amehsam69@gmail.com>
2023-12-06 22:42:24 +0100

    view-more bug fixed

--- main.js
+++ main.js
@@ -1,16 +1,10 @@
 const menuBar = document.querySelector("#menu-bar");
 const navLinks = document.querySelector(".nav");
 const viewMoreBtn = document.querySelector("#viewMore-btn");
-const loadMoreArticle = document.querySelector(".load-more-article");
-const featuredPostContainer = document.querySelector(
-  ".featured-post-container"
-);
+const loadMoreArticle = document.querySelectorAll(".load-more-article");
 
-console.log(featuredPostContainer);
 menuBar.addEventListener("click", openMenu);
-viewMoreBtn.addEventListener("click", function () {
-  loadMoreArticles();
-});
+viewMoreBtn.addEventListener("click", viewMoreArticle);
 
 function openMenu() {
   navLinks.classList.toggle("active");
@@ -21,45 +15,12 @@ function openMenu() {
   }
 }
 
-function loadMoreArticles() {
-  const articlesHTML = `
-  <article class="load-more-article home-article">
-  <img src="assets/img/blog-image-04.png" alt="" />
-  <p class="article-date">SEPTEMBER 23, 2023</p>
-  <h2 class="article-title">Blog four</h2>
-  <p class="article-body-preview">
-    I'm excited to start a new learning journey as a Scrimba Bootcamp
-    student! After several months of learning in the Frontend
-    Developer Career Path.
-  </p>
-</article>
+function viewMoreArticle() {
+  viewMoreBtn.textContent === "View More"
+    ? (viewMoreBtn.textContent = "View Less")
+    : (viewMoreBtn.textContent = "View More");
 
-<article class="load-more-article home-article">
-  <img src="assets/img/blog-image-05.png" alt="" />
-  <p class="article-date">SEPTEMBER 23, 2023</p>
-  <h2 class="article-title">Blog five</h2>
-  <p class="article-body-preview">
-    I'm excited to start a new learning journey as a Scrimba Bootcamp
-    student! After several months of learning in the Frontend
-    Developer Career Path.
-  </p>
-</article>
-
-<article class="load-more-article home-article">
-  <img src="assets/img/blog-image-06.png" alt="" />
-  <p class="article-date">SEPTEMBER 23, 2023</p>
-  <h2 class="article-title">Blog six</h2>
-  <p class="article-body-preview">
-    I'm excited to start a new learning journey as a Scrimba Bootcamp
-    student! After several months of learning in the Frontend
-    Developer Career Path.
-  </p>
-</article>
-    `;
-
-  featuredPostContainer.innerHTML += articlesHTML;
-
-  if (viewMoreBtn.style.display === "block") {
-    viewMoreBtn.style.display = "none";
-  }
+  loadMoreArticle.forEach((e) => {
+    e.classList.toggle("load-more");
+  });
 }
