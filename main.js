6fdcfa2b7056b1cb413e78afb843116357392cee
Samson Ameh <amehsam69@gmail.com>
2023-12-06 21:17:50 +0100

    Nav overflow and view-more btn bug not yet fixed

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
-let menuBar = document.querySelector("#menu-bar");
-let navLinks = document.querySelector(".nav");
+const menuBar = document.querySelector("#menu-bar");
+const navLinks = document.querySelector(".nav");
+const viewMoreBtn = document.querySelector("#viewMore-btn");
+const loadMoreArticle = document.querySelector(".load-more-article");
+const featuredPostContainer = document.querySelector(
+  ".featured-post-container"
+);
 
+console.log(featuredPostContainer);
 menuBar.addEventListener("click", openMenu);
+viewMoreBtn.addEventListener("click", function () {
+  loadMoreArticles();
+});
 
 function openMenu() {
   navLinks.classList.toggle("active");
@@ -9,5 +18,48 @@ function openMenu() {
     menuBar.style.color = "#ffffff";
   } else {
     menuBar.style.color = "";
+  }
+}
+
+function loadMoreArticles() {
+  const articlesHTML = `
+  <article class="load-more-article home-article">
+  <img src="assets/img/blog-image-04.png" alt="" />
+  <p class="article-date">SEPTEMBER 23, 2023</p>
+  <h2 class="article-title">Blog four</h2>
+  <p class="article-body-preview">
+    I'm excited to start a new learning journey as a Scrimba Bootcamp
+    student! After several months of learning in the Frontend
+    Developer Career Path.
+  </p>
+</article>
+
+<article class="load-more-article home-article">
+  <img src="assets/img/blog-image-05.png" alt="" />
+  <p class="article-date">SEPTEMBER 23, 2023</p>
+  <h2 class="article-title">Blog five</h2>
+  <p class="article-body-preview">
+    I'm excited to start a new learning journey as a Scrimba Bootcamp
+    student! After several months of learning in the Frontend
+    Developer Career Path.
+  </p>
+</article>
+
+<article class="load-more-article home-article">
+  <img src="assets/img/blog-image-06.png" alt="" />
+  <p class="article-date">SEPTEMBER 23, 2023</p>
+  <h2 class="article-title">Blog six</h2>
+  <p class="article-body-preview">
+    I'm excited to start a new learning journey as a Scrimba Bootcamp
+    student! After several months of learning in the Frontend
+    Developer Career Path.
+  </p>
+</article>
+    `;
+
+  featuredPostContainer.innerHTML += articlesHTML;
+
+  if (viewMoreBtn.style.display === "block") {
+    viewMoreBtn.style.display = "none";
   }
 }
