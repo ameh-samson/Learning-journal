ae51a730e7b350ef7187e88139e8a0adfcc483a3
Samson Ameh <amehsam69@gmail.com>
2023-12-07 16:28:12 +0100

    nav bar working fine after the merge

--- main.js
+++ main.js
@@ -5,28 +5,28 @@ const headerEl = document.querySelector(".header");
 const viewMoreBtn = document.querySelector("#viewMore-btn");
 const loadMoreArticle = document.querySelectorAll(".load-more-article");
 
-viewMoreBtn.addEventListener("click", viewMoreArticle);
-
 // MOBILE NAVIGATION FUNCTIONALITY | START
 btnNavEl.addEventListener("click", function () {
-    headerEl.classList.toggle("nav-open");
+  headerEl.classList.toggle("nav-open");
 });
 
+viewMoreBtn.addEventListener("click", viewMoreArticle);
+
 headerEl.addEventListener("click", function (event) {
-    // Check if the clicked element is a navigation link
-    if (event.target.classList.contains("nav-link")) {
-        // Close the mobile navigation
-        headerEl.classList.remove("nav-open");
-    }
+  // Check if the clicked element is a navigation link
+  if (event.target.classList.contains("nav-link")) {
+    // Close the mobile navigation
+    headerEl.classList.remove("nav-open");
+  }
 });
 
 // MOBILE NAVIGATION FUNCTIONALITY | END
 
 function viewMoreArticle() {
-    viewMoreBtn.textContent =
-        viewMoreBtn.textContent === "View More" ? "View Less" : "View More";
+  viewMoreBtn.textContent =
+    viewMoreBtn.textContent === "View More" ? "View Less" : "View More";
 
-    loadMoreArticle.forEach((e) => {
-        e.classList.toggle("load-more");
-    });
+  loadMoreArticle.forEach((e) => {
+    e.classList.toggle("load-more");
+  });
 }
